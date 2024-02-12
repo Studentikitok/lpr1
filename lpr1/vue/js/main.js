931f911e3ff1d90f8d59a247f281e09d85c7b672
Studentikitok <108453686+StraponBace@users.noreply.github.com>
2024-02-12 10:16:34 +0700

    add description

---
+++ lpr1/vue/js/main.js
@@ -0,0 +1,9 @@
+let product = "Socks";
+
+let app = new Vue({         //экземпляр vue
+    el: '#app',           //подключение экземпляра к элементу страницы
+    data: {                 //описание данных
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks"
+    }
+})
