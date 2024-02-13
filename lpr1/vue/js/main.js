d5c8d9a1e001d4555109877746baa4418f11fbca
gr414_potvi <ponomarev.tima2005@mail.ru>
2024-02-13 09:39:18 +0700

    del from cart

--- lpr1/vue/js/main.js
+++ lpr1/vue/js/main.js
@@ -9,13 +9,28 @@ let app = new Vue({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
-            }
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            },
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-    }
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        cart: 0,
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        delFromCart(){
+            this.cart -= 1
+        }
+    },
+
 })
+
